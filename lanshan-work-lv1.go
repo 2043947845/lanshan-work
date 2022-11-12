6c6c678b94c0cafe0b4d9ff7838f819e23daad43
2043947845 <116961545+2043947845@users.noreply.github.com>
2022-11-12 13:16:23 +0800

    2022214841-吴哲-lv1

--- lanshan-work-lv1.go
+++ lanshan-work-lv1.go
@@ -1,4 +1,5 @@
-package main //w！的时间复杂程度很大，我目前只能通过阶乘次将数组中的数一遍又一遍的排，竭尽全力了。
+// 第一版，涉及阶乘，运算时间久
+package main
 
 import (
 	"fmt"
@@ -14,7 +15,7 @@ func j(w int) int {
 }
 
 func main() {
-	var n, l, r, q, w, x int
+	var n, l, r, q, w int
 	fmt.Scanf("%v %v %v", &n, &l, &r)
 	a := make([]int, n)
 	//给数组里n个值赋值
@@ -27,12 +28,48 @@ func main() {
 		for t := 0; t < j(w); t++ { //进行w！次从头到尾的排序
 			for e := 0; e < w; e++ { //进行一次从头到尾的排序
 				if a[l+e] > a[l+1+e] { //对前后数比较并排序
-					x = a[l+e]
-					a[l+e] = a[l+1+e]
-					a[l+1+e] = x
+					a[l+e], a[l+1+e] = a[l+1+e], a[l+e]
 				}
 			}
 		}
+	}
+	for m := 0; m < n; m++ {
+		fmt.Printf("%v ", a[m])
+	}
+}
+
+//第二版，冒泡排序
+package main
+
+import "fmt"
+
+func bubbleSort(arr []int) []int {
+	length := len(arr)
+	for i := 0; i < length; i++ {
+		for j := 0; j < length-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
+func main() {
+	var n, l, r, q int
+	fmt.Scanf("%v %v %v", &n, &l, &r)
+	a := make([]int, n)
+	b := make([]int, r-l+1)
+	//给数组里n个值赋值
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%v", &q)
+		a[i] = q
+	}
+	for c := 0; c <= r-l; c++{//将a[r]~a[l]提出给b[]
+		b[c] = a[l+c]
+	}
+	bubbleSort(b)//对b[]冒泡排序，只排a[r]~[l]
+	for d := 0; d <= r-l; d++{//将排序好的a[r]~a[l]放回原数组
+		a[l+d] = b[d]
 	}
 	for m := 0; m < n; m++ {
 		fmt.Printf("%v ", a[m])
